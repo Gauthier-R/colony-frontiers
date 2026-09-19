cf048fc1bf31ca07f109d32afacc84ef019f3edf
Gauthier-R <gauthier.raimbault@gmail.com>
2026-09-19 16:56:47 +0200

    chore: restructuration à la racine et mise à jour du gitignore

--- Flutter/Diners/dinners_app_meals_refactored/lib/utils/url_helper/url_helper_stub.dart
+++
@@ -1,4 +0,0 @@
-Future<void> openExternalUrl(String url) async {
-  // Rien à faire sur mobile natif car ce bouton est invisible,
-  // ou on pourrait utiliser url_launcher ici si on en avait besoin sur mobile.
-}
